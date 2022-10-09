87b8c55adc410c1b87f244eb66c278c3121428bd
yukimasaki <woodenhoe@gmail.com>
2022-10-09 05:36:46 +0000

    production用の設定を追加

--- src/gas-account-book/vue.config.js
+++ src/gas-account-book/vue.config.js
@@ -1,5 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: isProd ? '/vue-gas-app' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
 }
